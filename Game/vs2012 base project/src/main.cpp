1653d7d4ae2473d925aaf3fea396930cfc0b3a2b
Youssef Hanna <youssefmyh@gmail.com>
2025-02-26 15:11:08 +0100

    Update main.cpp

--- Game/vs2012 base project/src/main.cpp
+++ Game/vs2012 base project/src/main.cpp
@@ -6,58 +6,9 @@
 #define new DEBUG_NEW
 #endif
 
-#include <king/Engine.h>
-#include <king/Updater.h>
 #include <stdlib.h>
 #include <crtdbg.h> 
 #include "../msvc/LevelScene.h"
-//**********************************************************************
-
-/*
-class ExampleGame : public King::Updater {
-public:
-
-	ExampleGame()
-		: mEngine("./assets")
-		, mRotation(0.0f)
-		, mYellowDiamondX(100.0f)
-		, mYellowDiamondY(100.0f) {
-	}
-
-	void Start() {
-		mEngine.Start(*this);
-	}
-
-	void Update() {
-		mEngine.Render(King::Engine::TEXTURE_GREEN, 650.0f, 100.0f);
-		mEngine.Render(King::Engine::TEXTURE_RED, 100.0f, 450.0f);
-		mEngine.Render(King::Engine::TEXTURE_BLUE, 650.0f, 450.0f);
-
-		mEngine.Write("Green", 650.0f, 140.0f);
-		mEngine.Write("Red", 100.0f, 490.0f);
-		mEngine.Write("Blue", 650.0f, 490.0f);
-
-		const char text[] = "This rotates at 5/PI Hz";
-		mRotation += mEngine.GetLastFrameSeconds();
-		mEngine.Write(text, mEngine.GetWidth() / 2.0f, mEngine.GetHeight() / 2.0f, mRotation * 2.5f);
-
-		if (mEngine.GetMouseButtonDown()) {
-			mYellowDiamondX = mEngine.GetMouseX();
-			mYellowDiamondY = mEngine.GetMouseY();
-		}
-		mEngine.Render(King::Engine::TEXTURE_YELLOW, mYellowDiamondX, mYellowDiamondY);
-		mEngine.Write("Click to", mYellowDiamondX, mYellowDiamondY + 0.0f);
-		mEngine.Write("move me!", mYellowDiamondX, mYellowDiamondY + 70.0f);
-	}
-
-private:
-	King::Engine mEngine;
-	float mRotation;
-	float mYellowDiamondX;
-	float mYellowDiamondY;
-};
-*/
-//**********************************************************************
 
 int main(int argc, char *argv[]) {
 
